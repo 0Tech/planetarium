474295d764c1ad29c123b7790a0cee8b78a790b6
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2023-09-17 08:32:54 +0000

    Guess build image tag

---
+++ util/docker.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+assert_docker() {
+	if ! docker info >/dev/null 2>&1
+	then
+		cmake -E echo "Error: You must have access to docker: add your uid to docker group"
+		false
+	fi
+}
+
+debian_codenames() {
+	cat <<EOF
+bookworm
+bullseye
+buster
+stretch
+jessie
+EOF
+}
+
+guess_image_tag() {
+	local name=$1
+	local version=$2
+
+	for _codename in $(debian_codenames)
+	do
+		_tag=$version-$_codename
+		if docker manifest inspect $name:$_tag >/dev/null
+		then
+			printf $_tag
+			return
+		fi
+	done
+}
+
+assert_docker
