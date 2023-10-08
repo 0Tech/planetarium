acd9dc1fc2c21a521fb0b3e398c3b9f8602f2a7a
Youngtaek Yoon <noreply@yoon.anonaddy.me>
2023-10-08 14:19:04 +0000

    Add app script for tests

---
+++ test/util/app.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+app_exec() {
+	local service_name=$1
+	local app=$2
+	local command=$3
+
+	local daemon=$(daemon $(app_name $app))
+
+	service_exec_as_user $service_name cosmovisor "$daemon $command"
+}
+
+query_tx() {
+	local service_name=$1
+	local app=$2
+	local txhash=$3
+
+	while ! _result=$(app_exec $service_name $app "query tx $txhash 2>/dev/null")
+	do
+		sleep 1
+	done
+	echo $_result
+}
+
+broadcast_tx() {
+	local service_name=$1
+	local app=$2
+	local command=$3
+
+	local response=$(app_exec $service_name $app "tx $command --gas auto --gas-adjustment 1.2 --yes")
+	[ -n "$response" ]
+
+	printf "$response" | jq -er .txhash
+}
